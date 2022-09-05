9af2c437f832b3caa0ccdb1200bf6633a50be3f7
Jatin-gumber12 <93000878+Jatin-gumber12@users.noreply.github.com>
2022-09-05 16:11:16 +0530

    Add files via upload

---
+++ Turtle day 3.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+goto(30,30)
+bgcolor("black")
+color("yellow")
+pensize(6)
+speed(5)
+
+for i in range(8):
+          right(45)
+          for i in range(8):
+                    back(100)
+                    right(45)
+hideturtle()
